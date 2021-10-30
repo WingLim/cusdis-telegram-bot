21f9734560880f0765688ee4d6eab474fd7b3a45
WingLim <winglim@qq.com>
2021-10-30 16:54:11 +0800

    refactor(bot): use filter to find reply message

--- api/bot.ts
+++ api/bot.ts
@@ -1,5 +1,5 @@
 import { VercelRequest, VercelResponse } from '@vercel/node'
-import { Bot, InlineKeyboard, webhookCallback } from 'grammy'
+import { Bot, InlineKeyboard, webhookCallback, Context, Filter } from 'grammy'
 import { client } from '../redis'
 import axios from 'axios'
 
@@ -7,16 +7,19 @@ const { BOT_TOKEN, VERCEL_URL } = process.env
 
 export const bot = new Bot(BOT_TOKEN)
 
+// Handle /start command
 bot.command('start', async (ctx) => {
     await ctx.reply('Welcome to use Cusdis Bot')
 })
 
+// Handle /gethook command
 bot.command('gethook', async (ctx) => {
     let chanId = ctx.message.chat.id
     let hookUrl = `https://${VERCEL_URL}/api/hook/${chanId}`
     await ctx.reply(`Your Webhook URL:\n ${hookUrl}`)
 })
 
+// Handle /about command
 bot.command('about', async (ctx) => {
     let links = new InlineKeyboard()
         .url('Github', 'https://github.com/WingLim/cusdis-telegram-bot').row()
@@ -26,12 +29,15 @@ bot.command('about', async (ctx) => {
     })
 })
 
+// Handle Approve button callback
 bot.callbackQuery('approve', async (ctx) => {
     let chatId = ctx.message.chat.id
     let key = chatId.toString() + ctx.message.message_id.toString()
     await client.connect()
+    // Get approve link from redis
     let link = await client.get(key)
     if (link) {
+        // Replace to api url
         let api = link.replace('open', 'api/open')
         let res = await axios.post(api)
         if (res.status == 200) {
@@ -48,27 +54,38 @@ bot.callbackQuery('approve', async (ctx) => {
     }
 })
 
-bot.on('msg', async (ctx) => {
-    if (ctx.message.reply_to_message) {
-        let reply_msg = ctx.message.reply_to_message
-        let key = ctx.chat.id.toString() + reply_msg.message_id.toString()
-        let replyContent = ctx.message.text
+type MessageContext = Filter<Context, 'msg'>
+type ReplyContext = MessageContext & {
+    message: {
+        reply_to_message: object
+    }
+}
 
-        await client.connect()
-        let link = await client.get(key)
-        if (link) {
-            let api = link.replace("open", "api/open")
-            let res = await axios.post(api, {
-                replyContent
-            })
-            if (res.status == 200) {
-                ctx.reply("Success to append comment")
-            } else {
-                ctx.reply("Fail to append comment")
-            }
+// hasReply is a filter to find the message which is reply to message
+function hasReply(ctx: MessageContext): ctx is ReplyContext {
+    return ctx.message.reply_to_message !== undefined
+}
+
+// Handle reply message to comment noticification
+bot.on('msg').filter(hasReply, async (ctx) => {
+    let reply_msg = ctx.message.reply_to_message
+    let key = ctx.chat.id.toString() + reply_msg.message_id.toString()
+    let replyContent = ctx.message.text
+
+    await client.connect()
+    let link = await client.get(key)
+    if (link) {
+        let api = link.replace("open", "api/open")
+        let res = await axios.post(api, {
+            replyContent
+        })
+        if (res.status == 200) {
+            ctx.reply("Success to append comment")
         } else {
-            ctx.reply("Token has expired")
+            ctx.reply("Fail to append comment")
         }
+    } else {
+        ctx.reply("Token has expired")
     }
 })
 
